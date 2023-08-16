05a2190fe6f43fd8841ba6441c49c7981f977956
w0no <jwh1401@dgu.ac.kr>
2023-08-17 00:04:31 +0900

    feat: 포즈넷 관련 util 함수 추가

---
+++ src/utils/posenet.js
@@ -0,0 +1,82 @@
+import "@tensorflow/tfjs-backend-webgl";
+import * as posenet from "@tensorflow-models/posenet";
+import * as tf from "@tensorflow/tfjs-core";
+
+const color = "aqua";
+const boundingBoxColor = "red";
+const lineWidth = 2;
+
+export function toTuple({ y, x }) {
+  return [y, x];
+}
+
+export function drawPoint(ctx, y, x, r, color) {
+  ctx.beginPath();
+  ctx.arc(x, y, r, 0, 2 * Math.PI);
+  ctx.fillStyle = color;
+  ctx.fill();
+}
+
+/**
+ * Draws a line on a canvas, i.e. a joint
+ */
+export function drawSegment(ay, ax, by, bx, color, scale, ctx) {
+  ctx.beginPath();
+  ctx.moveTo(ax * scale, ay * scale);
+  ctx.lineTo(bx * scale, by * scale);
+  ctx.lineWidth = lineWidth;
+  ctx.strokeStyle = color;
+  ctx.stroke();
+}
+
+/**
+ * Draws a pose skeleton by looking up all adjacent keypoints/joints
+ */
+export function drawSkeleton(keypoints, minConfidence, ctx, scale = 1) {
+  const adjacentKeyPoints = posenet.getAdjacentKeyPoints(
+    keypoints,
+    minConfidence
+  );
+
+  adjacentKeyPoints.forEach((keypoints) => {
+    const [ay, ax] = toTuple(keypoints[0].position);
+    const [by, bx] = toTuple(keypoints[1].position);
+    drawSegment(ay, ax, by, bx, color, scale, ctx);
+  });
+}
+
+/**
+ * Draw pose keypoints onto a canvas
+ */
+export function drawKeypoints(keypoints, minConfidence, ctx, scale = 1) {
+  for (let i = 0; i < keypoints.length; i++) {
+    const keypoint = keypoints[i];
+
+    if (keypoint.score < minConfidence) {
+      continue;
+    }
+
+    const { y, x } = keypoint.position;
+    drawPoint(ctx, y * scale, x * scale, 3, color);
+  }
+}
+
+/**
+ * Draw the bounding box of a pose. For example, for a whole person standing
+ * in an image, the bounding box will begin at the nose and extend to one of
+ * ankles
+ */
+export function drawBoundingBox(keypoints, ctx) {
+  const boundingBox = posenet.getBoundingBox(keypoints);
+
+  ctx.beginPath();
+  ctx.rect(
+    boundingBox.minX,
+    boundingBox.minY,
+    boundingBox.maxX - boundingBox.minX,
+    boundingBox.maxY - boundingBox.minY
+  );
+
+  ctx.strokeStyle = boundingBoxColor;
+  ctx.stroke();
+}
